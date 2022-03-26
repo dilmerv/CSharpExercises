bb43c57395a71874cc0198e3f347a458b6e44e56
Nolan Anderson <102380091+nolan-anderson@users.noreply.github.com>
2022-03-26 00:24:09 -0600

    added GoRESTApiUserInfo class

---
+++ StampinUpService/StampinUpService/Models/GoRESTApiUserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StampinUp.Service.Models
+{
+    public class GoRESTApiUserInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string status { get; set; }
+    }
+}
